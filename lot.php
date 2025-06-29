f78ad82fc6ce1e776f1a123bb45f39dbdedc7e31
Alexey <kiselev-academy@yandex.ru>
2025-06-29 13:23:50 +0300

    fix: add template for lot v2

--- lot.php
+++ lot.php
@@ -32,33 +32,28 @@ if (!isset($_GET['id'])) {
     print($layout_content);
     die();
 }
-else {
-    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-        if ($id !== null && $id !== false) {
-            $lots = get_lot_by_id($link, $id);
-            if (empty($lots)) {
-                $page_content = include_template('404.php', ['categories' => $categories]);
-                $layout_content = include_template('layout.php', [
-                'title' => $title,
-                'is_auth' => $is_auth,
-                'user_name' => $user_name,
-                'categories' => $categories,
-                'page_content' => $page_content
-                ]);
-                print($layout_content);
-                die();
-            }
-            else {
-                $page_content = include_template('lot.php', ['categories' => $categories, 'lots' => $lots]);
-                $layout_content = include_template('layout.php', [
-                'title' => $title,
-                'is_auth' => $is_auth,
-                'user_name' => $user_name,
-                'categories' => $categories,
-                'page_content' => $page_content
-                ]);
-                print($layout_content);
-                die();
-            }
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$lots = get_lot_by_id($link, $id);
+    if (empty($lots)) {
+        $page_content = include_template('404.php', ['categories' => $categories]);
+        $layout_content = include_template('layout.php', [
+        'title' => $title,
+        'is_auth' => $is_auth,
+        'user_name' => $user_name,
+        'categories' => $categories,
+        'page_content' => $page_content
+        ]);
+        print($layout_content);
+        die();
     }
-}
+    $page_content = include_template('lot.php', ['categories' => $categories, 'lots' => $lots]);
+    $layout_content = include_template('layout.php', [
+    'title' => $title,
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
+    'categories' => $categories,
+    'page_content' => $page_content
+    ]);
+    print($layout_content);
+    die();
+
--- models/lot.php
+++ models/lot.php
@@ -13,10 +13,13 @@ require_once 'init.php';
  */
 function get_new_lots($link): array
 {
-    $sql = 'SELECT l.id, l.name, l.start_price, l.img, l.date_end, b.price, c.name as category_name FROM lots l '
-     . 'JOIN categories c ON l.category_id = c.id '
-     . 'LEFT JOIN bets b ON l.id = b.lot_id '
-     . 'ORDER BY l.created_at DESC LIMIT 6';
+    $sql = <<<QUERY
+        SELECT l.id, l.name, l.start_price, l.img, l.date_end, b.price, c.name as category_name FROM lots l
+        JOIN categories c ON l.category_id = c.id
+        LEFT JOIN bets b ON l.id = b.lot_id
+        ORDER BY l.created_at DESC LIMIT 6
+    QUERY;
+
     return get_arr($link, $sql);
 }
 
@@ -28,8 +31,11 @@ function get_new_lots($link): array
  */
 function get_lot_by_id($link, $id): array
 {
-    $sql = 'SELECT l.*, c.name as category_name FROM lots l '
-    . 'JOIN categories c ON l.category_id = c.id '
-    . 'WHERE l.id = '. $id;
+    $sql = <<<QUERY
+        SELECT l.*, c.name as category_name FROM lots l
+        JOIN categories c ON l.category_id = c.id
+        WHERE l.id = $id
+    QUERY;
+
     return get_arr($link, $sql);
 }
